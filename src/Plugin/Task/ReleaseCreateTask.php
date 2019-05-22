4339906cd1271eccbc9d34b9a5088c151fd918fb
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-05-22 14:04:08 +0200

    Issue #1: New task: Release create

---
+++ src/Plugin/Task/ReleaseCreateTask.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace PhpTaskman\Github\Plugin\Task;
+
+use Github\Client;
+use Robo\Result;
+
+final class ReleaseCreateTask extends Github
+{
+    const ARGUMENTS = [
+        'project',
+        'tag',
+        'token',
+        'user',
+        'target',
+        'name',
+        'body',
+        'draft',
+        'prerelease',
+    ];
+
+    const NAME = 'github.release.create';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        $arguments = $this->getTaskArguments() +
+            [
+                'name' => null,
+                'body' => null,
+                'draft' => null,
+                'prerelease' => null,
+                'target' => null,
+                'tag' => null,
+            ];
+
+        $post = [
+            'tag_name' => $arguments['tag'],
+            'target_commitish' => $arguments['target'],
+            'name' => $arguments['name'],
+            'body' => \file_exists($arguments['body']) ? \file_get_contents($arguments['body']) : $arguments['body'],
+            'draft' => $arguments['draft'],
+            'prerelease' => $arguments['prerelease'],
+        ];
+        $post = \array_filter($post, 'strlen');
+
+        $github = new Client();
+        $github->authenticate($arguments['token'], null, Client::AUTH_URL_TOKEN);
+
+        try {
+            $github
+                ->repo()
+                ->releases()
+                ->create(
+                    $arguments['user'],
+                    $arguments['project'],
+                    $post
+                );
+        } catch (\Exception $e) {
+            return new Result($this, Result::EXITCODE_ERROR, $e->getMessage());
+        }
+
+        $this->printTaskInfo('Release has been created.');
+
+        return new Result($this, Result::EXITCODE_OK);
+    }
+}
